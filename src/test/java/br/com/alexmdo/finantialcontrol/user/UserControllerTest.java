c41e7104408c97903e841a3a9589b35920ca7b8c
Alexandre Oliveira <alexmdo@gmail.com>
2023-05-20 23:59:20 -0300

    Refactor to use Java's var inference syntax

--- src/test/java/br/com/alexmdo/finantialcontrol/user/UserControllerTest.java
+++ src/test/java/br/com/alexmdo/finantialcontrol/user/UserControllerTest.java
@@ -8,21 +8,17 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import br.com.alexmdo.finantialcontrol.user.dto.UserCreateRequestDto;
-import br.com.alexmdo.finantialcontrol.user.dto.UserDto;
 import br.com.alexmdo.finantialcontrol.user.dto.UserUpdateRequestDto;
 
 class UserControllerTest {
@@ -40,15 +36,15 @@ class UserControllerTest {
 
     @Test
     void testCreateUser() {
-        Long userId = 1L;
-        UserCreateRequestDto createRequestDto = new UserCreateRequestDto("John", "Doe", "johndoe@example.com");
-        User expectedUser = new User(userId, "John", "Doe", "johndoe@example.com");
+        var userId = 1L;
+        var createRequestDto = new UserCreateRequestDto("John", "Doe", "johndoe@example.com");
+        var expectedUser = new User(userId, "John", "Doe", "johndoe@example.com");
 
-        when(userService.createUser(any(User.class))).thenReturn(expectedUser);
+        when(userService.createUser(any())).thenReturn(expectedUser);
 
-        ResponseEntity<UserDto> response = userController.createUser(createRequestDto);
+        var response = userController.createUser(createRequestDto);
 
-        verify(userService, times(1)).createUser(any(User.class));
+        verify(userService, times(1)).createUser(any());
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertEquals(expectedUser.getId(), response.getBody().id());
         assertEquals(expectedUser.getFirstName(), response.getBody().firstName());
@@ -58,17 +54,15 @@ class UserControllerTest {
 
     @Test
     void testUpdateUser() {
-        Long userId = 1L;
-        UserUpdateRequestDto updateRequest = new UserUpdateRequestDto("John", "Doe", "johndoe@example.com");
-        User user = new User(userId, "John", "Doe", "johndoe@example.com");
+        var userId = 1L;
+        var updateRequest = new UserUpdateRequestDto("John", "Doe", "johndoe@example.com");
+        var expectedUser = new User(userId, "John", "Doe", "johndoe@example.com");
 
-        User expectedUser = new User(userId, "John", "Doe", "johndoe@example.com");
+        when(userService.updateUser(any())).thenReturn(expectedUser);
 
-        when(userService.updateUser(user)).thenReturn(expectedUser);
+        var response = userController.updateUser(userId, updateRequest);
 
-        ResponseEntity<UserDto> response = userController.updateUser(userId, updateRequest);
-
-        verify(userService, times(1)).updateUser(user);
+        verify(userService, times(1)).updateUser(any());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(expectedUser.getId(), response.getBody().id());
         assertEquals(expectedUser.getFirstName(), response.getBody().firstName());
@@ -78,9 +72,9 @@ class UserControllerTest {
 
     @Test
     void testDeleteUser() {
-        Long userId = 1L;
+        var userId = 1L;
 
-        ResponseEntity<Void> response = userController.deleteUser(userId);
+        var response = userController.deleteUser(userId);
 
         verify(userService, times(1)).deleteUser(userId);
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
@@ -88,12 +82,12 @@ class UserControllerTest {
 
     @Test
     void testGetUserById() {
-        Long userId = 1L;
-        User expectedUser = new User(userId, "John", "Doe", "johndoe@example.com");
+        var userId = 1L;
+        var expectedUser = new User(userId, "John", "Doe", "johndoe@example.com");
 
         when(userService.getUserById(userId)).thenReturn(expectedUser);
 
-        ResponseEntity<UserDto> response = userController.getUserById(userId);
+        var response = userController.getUserById(userId);
 
         verify(userService, times(1)).getUserById(userId);
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -105,23 +99,23 @@ class UserControllerTest {
 
     @Test
     void testGetAllUsers() {
-        List<User> expectedUserList = Arrays.asList(
+        var expectedUserList = Arrays.asList(
                 new User(1L, "John", "Doe", "johndoe@example.com"),
                 new User(2L, "Jane", "Smith", "janesmith@example.com"));
 
-        Pageable pageable = mock(Pageable.class);
-        Page<User> userPage = new PageImpl<>(expectedUserList, pageable, expectedUserList.size());
+        var pageable = mock(Pageable.class);
+        var userPage = new PageImpl<>(expectedUserList, pageable, expectedUserList.size());
 
-        when(userService.getAllUsers(pageable)).thenReturn(userPage);
+        when(userService.getAllUsers(any())).thenReturn(userPage);
 
-        ResponseEntity<Page<UserDto>> response = userController.getUsers(pageable);
+        var response = userController.getUsers(pageable);
 
-        verify(userService, times(1)).getAllUsers(pageable);
+        verify(userService, times(1)).getAllUsers(any());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(expectedUserList.size(), response.getBody().getSize());
         for (int i = 0; i < expectedUserList.size(); i++) {
-            User expectedUser = expectedUserList.get(i);
-            UserDto userDto = response.getBody().getContent().get(i);
+            var expectedUser = expectedUserList.get(i);
+            var userDto = response.getBody().getContent().get(i);
             assertEquals(expectedUser.getId(), userDto.id());
             assertEquals(expectedUser.getFirstName(), userDto.firstName());
             assertEquals(expectedUser.getLastName(), userDto.lastName());
