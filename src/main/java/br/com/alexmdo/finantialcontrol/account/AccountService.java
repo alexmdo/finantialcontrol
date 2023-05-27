119eda637814cff595764afd97ea7d8251631b47
Alexandre Oliveira <alexmdo@gmail.com>
2023-05-26 23:28:11 -0300

    Model account domain

---
+++ src/main/java/br/com/alexmdo/finantialcontrol/account/AccountService.java
@@ -0,0 +1,49 @@
+package br.com.alexmdo.finantialcontrol.account;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Account updateAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public void deleteAccount(Long id) {
+        Account account = getAccountById(id);
+        if (account.isArchived()) {
+            accountRepository.delete(account);
+        } else {
+            throw new IllegalArgumentException("Cannot delete account. Archive it first.");
+        }
+    }
+
+    public Account getAccountById(Long id) {
+        return accountRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Account not found with id: " + id));
+    }
+
+    public Account archiveAccount(Long id) {
+        Account account = getAccountById(id);
+        account.setArchived(true);
+        return accountRepository.save(account);
+    }
+
+    public Account updateInitialAmount(Long id, BigDecimal amount) {
+        Account account = getAccountById(id);
+        account.setInitialAmount(amount);
+        return accountRepository.save(account);
+    }
+
+}
---
+++ src/main/java/br/com/alexmdo/finantialcontrol/account/AccountType.java
@@ -0,0 +1,9 @@
+package br.com.alexmdo.finantialcontrol.account;
+
+public enum AccountType {
+    CHECKING_ACCOUNT,
+    MONEY,
+    SAVING_ACCOUNT,
+    INVESTMENTS,
+    OTHER
+}
